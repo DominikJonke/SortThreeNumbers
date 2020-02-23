7c1f34be6c64910ea3d846ed2e20b6e825b4aa77
D J <dominik.jonke@outlook.com>
2020-02-23 14:38:45 +0100

    Sort method (a < b < c)

---
+++ SortThreeNumbers/SortThreeNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SortThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortNumbers.ShowTitle();
+
+            SortNumbers.SetNumber();
+
+            SortNumbers.SortNumber();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SortThreeNumbers/SortThreeNumbers/SortNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SortThreeNumbers
+{
+    public static class SortNumbers
+    {
+        private static int one = 0;
+        private static int two = 0;
+        private static int three = 0;
+        public static void ShowTitle()
+        {
+            Console.WriteLine("\n\t\t\tSort Numbers\n");
+        }
+        public static void SetNumber()
+        {
+            Console.Write("\tFirst number:\t");
+            one = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\tFirst number:\t");
+            two = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\tFirst number:\t");
+            three = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static void SortNumber()
+        {
+            if(one > two && one > three)
+            {
+                if (three > two)
+                {
+                    Console.Write($"\n\t\t{two}, {three}, {one}");
+                }
+                else
+                {
+                    Console.Write($"\n\t\t{three}, {two}, {one}");
+                }
+            }
+            else if(two > one && two > three)
+            {
+                if(one > three)
+                {
+                    Console.Write($"\n\t\t{three}, {one}, {two}");
+                }
+                else
+                {
+                    Console.Write($"\n\t\t{one}, {three}, {two}");
+                }
+            }
+            else if(one > two)
+            {
+                Console.Write($"\n\t\t{two}, {one}, {three}");
+            }
+        }
+    }
+}
